dc4d03abd267cf91b9558480cd6127bf883db567
Vaughn Pole <vjp2109@columbia.edu>
2021-06-26 12:18:39 -0400

    finished cycle2

--- db/models/test.js
+++ db/models/test.js
@@ -2,6 +2,20 @@ const Sequelize = require('sequelize');
 const db = require('../db');
 const Student = require('./student');
 
-const Test = db.define();
+const Test = db.define('test', {
+  subject: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  grade: {
+    type: Sequelize.INTEGER,
+    allowNull: false
+  }
+});
+
+//Associations
+Test.belongsTo(Student);
+Student.hasMany(Test);
+
 
 module.exports = Test;
