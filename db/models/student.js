b8294bf7c5ab2826fe081d328c4734f7326f2412
Vaughn Pole <vjp2109@columbia.edu>
2021-06-26 11:15:16 -0400

    most of it

--- db/models/student.js
+++ db/models/student.js
@@ -1,6 +1,25 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
 
-const Student = db.define();
+const Student = db.define('student', {
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true
+    }
+  }
+});
+
+
+
 
 module.exports = Student;
